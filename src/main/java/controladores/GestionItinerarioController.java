c8561b49062e1068e7c8867117187015cbcbc30a
KASSANDRA JEZEBEL GAITAN <k03438034@gmail.com>
2025-03-19 17:03:19 +0100

    Primer commit

--- src/main/java/controladores/GestionItinerarioController.java
+++ src/main/java/controladores/GestionItinerarioController.java
@@ -35,42 +35,6 @@ public class GestionItinerarioController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        ObservableList<Itinerario> itinerarios = FXCollections.observableArrayList(
-                new Itinerario("Volcanes y Lagunas", "Explora los famosos volcanes y lagunas de Nicaragua", "/img/volcanes.jpg"),
-                new Itinerario("Colonial y Playas", "Descubre la belleza colonial de Granada y las playas del Pacífico", "/img/granada.jpg"),
-                new Itinerario("Aventura en Isla de Ometepe", "Aventura y naturaleza en la impresionante Isla de Ometepe", "/img/ometepe.jpg")
-        );
-
-        listaItinerario.setItems(itinerarios);
-        listaItinerario.setCellFactory(param -> new ListCell<Itinerario>() {
-            @Override
-            protected void updateItem(Itinerario itinerario, boolean empty) {
-                super.updateItem(itinerario, empty);
-
-                if (empty || itinerario == null) {
-                    setGraphic(null);
-                } else {
-                    try {
-                        FXMLLoader loader = new FXMLLoader(getClass().getResource("/vistas/Itinerario_Item.fxml"));
-                        HBox hbox = loader.load();
-
-                        Label lblNombre = (Label) hbox.lookup("#lblNombre");
-                        Label lblDescripcion = (Label) hbox.lookup("#lblRol"); // Renombrado a lblDescripcion para claridad
-                        ImageView imgItinerario = (ImageView) hbox.lookup("#imgUsuario"); // Renombrado a imgItinerario
-
-                        lblNombre.setText(itinerario.getNombre());
-                        lblDescripcion.setText(itinerario.getDescripcion());
-                        imgItinerario.setImage(new Image(itinerario.getFoto()));
-
-                        setGraphic(hbox);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
-
-
-
+       
     }
 }
